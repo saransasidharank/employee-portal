9e024b7bfc5ab3dbb645e3415e0e45a44363962d
saransasidharank <saransasidharan07@gmail.com>
2024-02-29 16:33:43 +0530

    server completd

---
+++ index.js
@@ -0,0 +1,12 @@
+const jsonServer = require('json-server')
+const empServer = jsonServer.create()
+const middleware = jsonServer.defaults()
+const router = jsonServer.router("db.json")
+const PORT = 3000 || process.env.PORT
+
+empServer.use(middleware)
+empServer.use(router)
+
+empServer.listen(PORT,()=>{
+    console.log((`Employee Portal Started at port: ${PORT}`));
+})
